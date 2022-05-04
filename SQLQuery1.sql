d41c38b3a3df1716018bda8514f8594b937379a4
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-04 21:58:05 +0530

    [Manoj]Adding_UC3_CreateEmployeePayrollData

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,4 +8,10 @@ Name varchar(200),
 Salary float,
 StartDate datetime
 );
+----------UC3----------
+insert into employeee_payroll(Name,Salary)
+values('Nagasaimanoj',40000),('Jonamiasagan',50000);
+--passing data to akk columns so nin need t specify column names--
+insert into employeee_payroll
+values('NM',4561,'2022-02-01'),('MN',4561,'2022-02-01');
 

d670f0d1e9012e5f8b0e5473109b9fb298c544b1
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-04 21:55:25 +0530

    [Manoj]adding_UC2_CreateEmployeePayrollTable

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,11 @@
 -----------UC1---------
 create database PayRoll;
 use PayRoll;
+-----------UC2----------
+create table employeee_payroll(
+id int identity(1,1) primary key,
+Name varchar(200),
+Salary float,
+StartDate datetime
+);
 
